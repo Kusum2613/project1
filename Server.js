8a0155bb7ee45806431d20a0422ef0f45cf16f12
kusum kumari 26 <138245167+Kusum2613@users.noreply.github.com>
2024-10-23 21:52:54 +0530

    Update Server.js

--- Server.js
+++ Server.js
@@ -2,7 +2,7 @@ const express=require('express');
 const app=express();
 const path=require('path');
 const port = process.env.PORT || 4000;
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'Landing-page/public')));
 app.get('/',(req,res)=>{
     res.sendFile(path.resolve(__dirname,'Landing-page/project1.html'));
 })
